23e6a87f314ed2620e21d5e6ee7e7da9b77eb742
Klemenxer <46409468+Klemenxer@users.noreply.github.com>
2019-02-03 00:13:31 +0800

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -12,7 +12,7 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Trial SSH Account====" | lolcat
+echo -e "====Trial SSH Account===="
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH: 22,143"
 echo -e "Port Dropbear: 80,443"
@@ -20,6 +20,6 @@ echo -e "Port Squid: 8080,3128"
 echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
 echo -e "Username: $Login"
 echo -e "Password: $Pass\n"
-echo -e "=========================" | lolcat
-echo -e "Mod by shigeno"
+echo -e "========================="
+echo -e ""
 echo -e ""

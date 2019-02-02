5ff5ca622616f3f16a3f4c0239d521d51b7da879
Klemenxer <46409468+Klemenxer@users.noreply.github.com>
2019-02-03 00:08:51 +0800

    Stabilization Fix

--- about.sh
+++ about.sh
@@ -2,7 +2,7 @@
 #About this script
 
 clear
-echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e "Script Auto Install SSH & OpenVPN v2.0"
 echo -e ""
 echo -e "For Debian 8 32 bit & 64 bit"
 echo -e "For VPS with KVM and VMWare virtualization"
@@ -12,9 +12,9 @@ echo -e "* Fornesia"
 echo -e "* Rzengineer"
 echo -e "* Fawzya"
 echo -e ""
-echo -e "Mod by shigeno"
+echo -e "MOD by Klemenxer"
 echo -e ""
-echo -e "https://www.phcorner.net/members/446256/"
+echo -e ""
 echo -e ""
 echo -e ""
 echo -e ""
